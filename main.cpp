a45e44bf5e147e52530b145492c48f176c4ea429
szeballos-m <sm_zeballos@hotmail.com>
2026-03-07 16:57:50 +0000

    Configuracion inicial

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nombre;
+    cout << "Ingresa tu nombre: ";
+    cin >> nombre;
+    cout << "Hola, " << nombre << "!" << endl;
+    return 0;
+}
